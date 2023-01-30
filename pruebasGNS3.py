4e343dab01e41b0902dba8e15bdaf2bfe7170241
marcocacho <marcocachosanchez@gmail.com>
2023-01-30 18:09:51 +0000

    Añadimos una funcion para abrir un proyecto dado o crearlo sino existe

--- pruebasGNS3.py
+++ pruebasGNS3.py
@@ -10,7 +10,9 @@ def openProyect(conector, nombre):
         lab.create()
     print(f"Se abre el proyecto {nombre}.")
     return lab
-def main(lab):
+#Crea un enlace entre dos Switches
+#Es una funcion estatica
+def CreateLinks(lab, server):
 
     # Añadimos un shitch al proyecto
     print("Lista de switches:")
@@ -18,14 +20,51 @@ def main(lab):
         #if "switch" in template["name"]:
             print(f"Template: {template['name']} -- ID: {template['template_id']}")
 
-    lab.create_node(name = 'test-switch01', template = 'Ethernet switch')
+    #crea un nodo con el nombre dado y del tipo proporcionado en el template
+    #lab.create_node(name = 'test-switch2', template = 'Ethernet switch')
 
+    nodos = lab.nodes
+    #listamos los nodos del proyecto
+    for node in lab.nodes:
+        print(f"Node: {node.name} -- Node Type: {node.node_type} -- Status: {node.status}")
+    """
+    for nodo in nodos:
+        print(nodo.ports)
+    """
+    #creamos un link entre dos nodos, se necesita conocer: node_id, adapter_number, port_number
+    #Se seleciona los elementos
+    nodes = [
+        dict(node_id=nodos[0].node_id, adapter_number=0, port_number=0),
+        dict(node_id=nodos[1].node_id, adapter_number=0, port_number=0)]
+
+    #Se crea el enlace entre los nodos
+    #link = Link(project_id=lab.project_id, connector=server, nodes=nodes)
+    #link.create()
+
+#recibe la ruta de una maquina virtual y la añade a gns3 para poder crear nodos.
+def AddVitualPC(nombre, ruta, server):
+    server.create_template(name=nombre, template_type="qemu", hda_disk_image=ruta)
+
+def main(lab, server):
+    print("Nodos posibles del servidor")
+    for template in server.get_templates():
+        print(f"Template: {template['name']} -- ID: {template['template_id']}")
+
+    print("Nodos en el del proyecto")
+    for node in lab.nodes:
+        print(f"Node: {node.name} -- Node Type: {node.node_type} -- Status: {node.status}")
 
 
 if __name__ == '__main__':
     # Inicializamos la API de GNS3
     gns3_server = Gns3Connector(url="http://127.0.0.1:3080")
     lab: Project = openProyect(gns3_server, "test_lab")
-    main(lab)
+    main(lab, gns3_server)
+
+    #localizacion maquinas virtuales por defecto (/var/lib/libvirt/nombreMaquina), permisos de superusuario
+    #AddVitualPC("maquina prueba", "/var/lib/libvirt/images/ubuntu20.04.qcow2", gns3_server)
+    AddVitualPC("Permisos777", "/home/marco/ubuntu20.04.qcow2", gns3_server)
+
+
 
 
