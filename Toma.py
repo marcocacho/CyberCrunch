6acb3d9da24f29b903933196017f8bf974df5195
Marco Cacho <48019295+marcocacho@users.noreply.github.com>
2023-02-14 19:48:35 +0100

    Update Toma.py
    tomad 2

--- Toma.py
+++ Toma.py
@@ -1,3 +1,12 @@
+import virtualbox
+
+# crear una instancia de la sesión de VirtualBox
+vbox = virtualbox.VirtualBox()
+
+# abrir el archivo VDI y agregarlo a VirtualBox
+path_to_vdi = '/ruta/al/archivo.vdi'
+medium = vbox.openMedium(path_to_vdi, virtualbox.DeviceType_HardDisk, virtualbox.AccessMode_ReadWrite, False)
+
 # crear una máquina virtual y configurarla con el archivo VDI agregado
 machine_name = "NOMBRE_DE_LA_MAQUINA"
 machine = vbox.createMachine("", machine_name, [], 'Linux', '')
