03196d59fa5eb2adcb75590d4af6d5f239a103aa
marco <marcocachosanchez@gmail.com>
2023-02-18 16:23:07 +0000

    Se añade una funcion para mover archivos de lugar

--- ConexionMaquinas.py
+++ ConexionMaquinas.py
@@ -9,13 +9,16 @@ Parametros:
     + host: maquina a la que nos queremos conectar
     + puerto: puerto donde se encuentra el protocolo ssh (por defecto el 22)
 """
-def conectSSH(user, password,host, puerto=22):
-#Se puede hacer lo mismo sin contraseña si se crea con la ceracion de llaves
+
+
+def conectSSH(user, password, host, puerto=22):
+    # Se puede hacer lo mismo sin contraseña si se crea con la ceracion de llaves
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, puerto, user, password)
 
     return ssh
+
 
 """
 Añadade un fichero a un equipo por scp
@@ -24,11 +27,35 @@ Parametros de entrada:
     + localFile: fichero lccal que se quiere subir
     + hostRute: ruta donde se almacenara el fichero
     + Nombre que queremos dar al fichero
-    + ssh: conexion ssh de la maquina que queremos conectarnos
+    + ssh: conexion ssh de la maquina 
+
+"""
+
+
+def addFile(localFile, hostRute, name, ssh):
+    scp = ssh.open_sftp()
+    scp.put(localFile, hostRute + '/' + name)
 
 
 """
-def addFile(localFile, hostRute, name, ssh):
+Mueve un fichero de ruta en el equipo destino, si en el lugar destino existe este se eliminara
 
-    scp = ssh.open_sftp()
-    scp.put(localFile, hostRute+'/'+name)
+Prerequisito que el usuario con el que nos conectamos se encuentre dentro del grupo de Sudo 
+o pueda ejecutar el comando mv como superususareio
+
+Parametros de entrada:
+    + origin: localizacion del fichero en el host
+    + destiny: Nuevo lugar del fichero
+    + ssh: conexion ssh de la maquina
+    + password: contraseña del usuario conectado
+"""
+def moveFile(origin, destiny, ssh, password):
+    comando = "echo " + password + " | sudo -S mv " + origin + ' ' + destiny
+    ssh.exec_command(comando)
+
+
+if __name__ == '__main__':
+    ssh = conectSSH("osboxes", "osboxes.org", "192.168.181.131")
+    # addFile("README.md", "/home/osboxes", "prueba", ssh)
+
+    moveFile("/home/osboxes/prueba", "/prueba", ssh, "osboxes.org")
