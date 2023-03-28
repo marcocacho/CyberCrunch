6ecb634bf1e50e3a450ed296f53dedec5abfb15f
marco <marcocachosanchez@gmail.com>
2023-03-28 18:46:56 +0000

    Añadidas funciones varias para control y reparadao algun comentario

--- Control.py
+++ Control.py
@@ -1,18 +1,24 @@
 
 def manageMachines(name, lab, action):
     """
-    Funcion encargada de encender, apagar o reiniciar una maquina
+    Funcion encargada de modifica una maquina de un proyencto de gns3
     :param name: nombre del nodo de la red sobre el que se quiere actuar
     :param lab: laboratorio de gns3
     :param action: accion que se quiere realizar (start, stop, reload, suspend, delete, status)
     :return: None
     """
     node = lab.get_node(name)
-    action_dict = {"start": node.start(),
-                   "stop": node.stop(),
-                   "reload": node.reload(),
-                   "suspend": node.suspend(),
-                   "delete": node.delete()}
-    action_dict[action]
-
+    if node is not None:
+        action_dict = {"start": node.start,
+                       "stop": node.stop,
+                       "reload": node.reload,
+                       "suspend": node.suspend,
+                       "delete": node.delete}
+        select = action_dict.get(action)
+        if select is not None:
+            select()
+        else:
+            print("No se encontro accion " + action)
+    else:
+        print("No existe el nodo seleccionado")
 
